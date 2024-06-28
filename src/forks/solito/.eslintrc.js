d155494624025a1a7cdac45e74a7abcc8f825f6d
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2024-06-28 16:11:00 -0400

    Untrack Solito files

--- src/forks/solito/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-const lint = require('expo-module-scripts/eslintrc.base.js')
-module.exports = {
-  ...lint,
-  rules: {
-    ...lint.rules,
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/array-type': 'off',
-  },
-}
