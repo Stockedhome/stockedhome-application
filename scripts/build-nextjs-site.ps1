afa99415a9d6177c3f3f23a355de96b8d70bbebd
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2024-10-13 22:51:13 -0400

    Working Builds?

--- scripts/build-nextjs-site.ps1
+++ scripts/build-nextjs-site.ps1
@@ -1,10 +1,6 @@
 $ErrorActionPreference = "Stop"
 Set-Location $PSScriptRoot/..
 
-rm ./node_modules/lib
-rm ./node_modules/interface
-pnpm install --frozen-lockfile --ignore-scripts
-
 pnpm exec next build src/platforms/next
 if ($?) {
     Write-Host "Next.js build successful" -ForegroundColor Green
@@ -18,7 +14,8 @@ if (!(Test-Path -Path "./dist/web-server")) {
     New-Item -ItemType Directory -Path "./dist/web-server"
 }
 
-Remove-Item -Path "./dist/web-server/*" -Recurse -Force
+Remove-Item -Path "./dist/web-server" -Recurse -Force
+New-Item -ItemType Directory -Path "./dist/web-server"
 
 # Copy the supabase_prod files
 Copy-Item -Path "./supabase_prod/*" -Destination "./dist/web-server" -Recurse -Force
@@ -47,3 +44,17 @@ if ($?) {
     Write-Host "Second Docker build (with-static) failed" -ForegroundColor Red
     exit 1
 }
+
+Remove-Item -Path "./dist/web-server-static" -Recurse -Force
+New-Item -ItemType Directory -Path "./dist/web-server-static"
+
+# Put static files into a directory, ready to be served by a CDN
+
+# For some reason, the Coopy-Item throws an error the first time I run it but works the second time.
+# Don't quite know why that is. If anyone can explain "Copy-Item: Container cannot be copied onto existing leaf item.", hit me up
+try {
+    Copy-Item -Path "./src/platforms/next/.next/static/*" -Destination "./dist/web-server-static/_next/static" -Recurse -Force
+} catch {
+    Copy-Item -Path "./src/platforms/next/.next/static/*" -Destination "./dist/web-server-static/_next/static" -Recurse -Force
+}
+Copy-Item -Path "./src/platforms/next/public/*" -Destination "./dist/web-server-static" -Recurse -Force
